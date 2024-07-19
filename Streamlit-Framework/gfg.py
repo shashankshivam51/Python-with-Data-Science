2956783b9bc250dd48d45075bff0860a96bc1d9b
Shashank Shivam <shashankshivam51@gmail.com>
2024-07-19 23:42:08 +0530

    This commit is after stramlit classification

--- Streamlit-Framework/gfg.py
+++ Streamlit-Framework/gfg.py
@@ -1,17 +1,6 @@
 import streamlit as stm 
-from streamlit_card import card 
   
   
 stm.set_page_config(page_title="This is a Simple Streamlit WebApp") 
 stm.title("This is the Home Page Geeks.") 
 stm.text("Geeks Home Page") 
-  
-  
-# Card 
-  
-card( 
-    title="Hello Geeks!", 
-    text="Click this card to redirect to GeeksforGeeks", 
-    image="https://media.geeksforgeeks.org/wp-content/cdn-uploads/20190710102234/download3.png", 
-    url="https://www.geeksforgeeks.org/", 
-) 
